f8553e38cda58e937ae2f3417aa1297161e7bd8c
vitormarquesr <96251229+vitormarquesr@users.noreply.github.com>
2023-11-12 19:54:05 -0300

    + LICENSE, README

---
+++ README.md
@@ -0,0 +1,5 @@
+# walkg
+
+## Overview
+
+walkg is a Python package for investigating various types of walks on graphs, such as random and quantum walks, either discrete-time or continuous-time.
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="walkg",
     version="0.1.0",
     author="Vitor Marques",
+    license='MIT',
     packages=find_packages(include=["walkg", "walkg.*"]),
-    install_requires=["scipy", "numpy"],
+    install_requires=["scipy", "numpy"]
 )
